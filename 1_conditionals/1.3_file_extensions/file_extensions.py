46c28fe68a8c7910cbd8f31e002355d844a870a8
bukhanovskayamarina22 <bukhanovskayamarina22@protonmail.com>
2024-04-22 19:44:23 +0300

    done the File Extensions problem

---
+++ 1_conditionals/1.3_file_extensions/file_extensions.py
@@ -0,0 +1,34 @@
+def main():
+    # greet (take user input)
+    filename = input('Filename with extension: ')
+    
+    # pring media type
+    print(getMediaType(filename.strip()))
+
+def getMediaType(filename):
+    # getting the filename extension and matching it to possible values
+    match getExtension(filename):
+        case '.gif':
+            return 'image/gif'
+        case '.jpg' | '.jpeg':
+            return 'image/jpeg'
+        case '.png':
+            return 'image/png'
+        case '.pdf':
+            return 'application/pdf'
+        case '.txt':
+            return 'text/plain'
+        case '.zip':
+            return 'application/zip'
+        # if no matching case found
+        case _: 
+            return 'application/octet-stream '
+
+
+def getExtension(filename):
+    # slice the string from the index of the first occurence of "." to the end of the string
+    # (getting the extension of the file)
+    return filename[filename.find('.'):]
+
+# entry point of the app 
+main()
